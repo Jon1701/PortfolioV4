85beb93428b5654cb6cdfe75ac9ed4a0841877b0
Jon Balon <development@jonbalon.com>
2019-02-02 23:01:22 -0500

    Restructure Webpack Configuration File for Adding Plugins Based on `NODE_ENV`
    - update `webpack.config.js` to allow for dynamically adding plugins
    based on the NODE_ENV variable

--- webpack.config.js
+++ webpack.config.js
@@ -13,36 +13,49 @@ const PATHS = {
   DEST: path.join(__dirname, 'dist'),
 }
 
-const webpackConfig = {
-  mode: process.env.NODE_ENV,
-  entry: path.join(PATHS.SRC, 'pages', 'index'),
-  resolve: {
-    extensions: ['.js', '.jsx']
-  },
-  module: {
-    rules: [
-      {
-        test: /\.jsx?$/,
-        exclude: /node_modules/,
-        loader: 'babel-loader',
-      }
-    ],
-  },
-  plugins: [
+module.exports = () => {
+  // Get Node environment variable.
+  const { NODE_ENV } = process.env;
+  const isProduction = NODE_ENV === CONSTANTS.NODE_ENV.PRODUCTION;
+
+  // Base set of plugins.
+  const plugins = [
     new HtmlWebpackPlugin({
       template: require('html-webpack-template'),
       inject: false,
       appMountId: 'app',
     }),
-  ],
-  devServer: {
-    contentBase: PATHS.DEST,
-    compress: true,
-    open: false,
-    port: 8080,
-    clientLogLevel: 'warning',
-    stats: 'errors-only',
-  },
+  ];
+
+  return {
+    mode: process.env.NODE_ENV,
+  
+    entry: path.join(PATHS.SRC, 'pages', 'index'),
+  
+    resolve: {
+      extensions: ['.js', '.jsx']
+    },
+  
+    module: {
+      rules: [
+        {
+          test: /\.jsx?$/,
+          exclude: /node_modules/,
+          loader: 'babel-loader',
+        }
+      ],
+    },
+  
+    devServer: {
+      contentBase: PATHS.DEST,
+      compress: true,
+      open: false,
+      port: 8080,
+      clientLogLevel: 'warning',
+      stats: 'errors-only',
+    },
+  
+    plugins
+  } 
 }
 
-module.exports = webpackConfig;
