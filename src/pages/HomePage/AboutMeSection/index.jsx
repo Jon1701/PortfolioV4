1e5813fbfc547f12353a9e01bfc3e1579b7aa2c9
Jon Balon <development@jonbalon.com>
2019-02-08 00:16:35 -0500

    Homepage About Me Section
    - add `About Me` section to the homepage
    - add static assets
      - background
      - SVG icons

---
+++ src/pages/HomePage/AboutMeSection/index.jsx
@@ -0,0 +1,121 @@
+import React from 'react';
+import styled from 'styled-components';
+import SVGIcon from '_components/SVGIcon';
+
+const bgImage = require('_backgrounds/hixs_pattern_evolution_@2X.png');
+
+const Section = styled.section`
+  // Positioning.
+  position: relative;
+
+  // Dimensions
+  max-width: 100vw;
+  min-height: 100vh;
+  box-sizing: border-box;
+
+  // Alignment
+  text-align: center;
+
+  // Font
+  font-family: 'Roboto', sans-serif;
+  font-size: 1rem;
+
+  // Background
+  background-color: #222d32;
+  background-image: url(${bgImage});
+  background-attachment: fixed;
+`;
+
+const ContentMargin = styled.div`
+  max-width: 892px;
+  margin: 0 auto;
+  padding: 25px 0;
+`;
+
+const SectionHeader = styled.h1`
+  font-size: 3rem;
+  font-weight: 100;
+`;
+
+const TechnologiesLayout = styled.div`
+  // Layout.
+  display: flex;
+  flex-flow: row wrap;
+  justify-content: space-evenly;
+
+  // Spacing.
+  margin: 25px 0;
+
+  // Icon container.
+  & > div {
+    flex-basis: 150px;
+
+    &:not(:last-child) {
+      margin-bottom: 35px;
+    }
+  }
+
+  // Icon.
+  & > div > div:first-child {
+    svg {
+      width: auto;
+      height: 60px;
+    }
+  }
+
+  // Label.
+  & > div > div:last-child {
+    font-size: 1rem;
+  }
+`;
+
+const AboutMeSection = () => {
+  const technologies = [
+    'html5',
+    'css3',
+    'javascript',
+    'react',
+    'redux',
+    'reactRouter',
+    'jquery',
+    'd3',
+    'chartjs',
+    'docker',
+    'git',
+    'webpack',
+    'gulp',
+    'sass',
+    'postcss',
+    'nodejs',
+    'mongodb',
+    'mysql',
+    'heroku',
+    'mocha',
+    'eslint',
+    'stylelint',
+    'expressjs',
+    'php',
+    'wordpress',
+  ];
+
+  return (
+    <Section>
+      <ContentMargin>
+        <SectionHeader>About Me</SectionHeader>
+        <p>
+          Hi! My name is Jon, and I am a Front-End Developer based in Toronto.
+        </p>
+
+        <p>Here are some of the technologies I have used:</p>
+
+        <TechnologiesLayout>
+          {technologies.map(item => (
+            <SVGIcon icon={item} />
+          ))}
+        </TechnologiesLayout>
+      </ContentMargin>
+    </Section>
+  );
+};
+
+export default AboutMeSection;
---
+++ src/pages/HomePage/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+// Page sections.
+import IntroSection from './IntroSection';
+import AboutMeSection from './AboutMeSection';
+
+const HomePage = () => (
+  <div>
+    <IntroSection />
+    <AboutMeSection />
+  </div>
+);
+
+export default HomePage;
