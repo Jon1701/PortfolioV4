79a44594efb46434ad1ce273ae4e500adb627a4f
Jon Balon <development@jonbalon.com>
2019-02-13 14:47:55 -0500

    Load Global Styles via Sass
    - remove global styles from `styled-components`
    - leverage webpack loaders instead

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,34 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createGlobalStyle } from 'styled-components';
 import HomePage from './HomePage';
 
-const GlobalStyle = createGlobalStyle`
-  body {
-    // Load fonts.
-    @import url('https://fonts.googleapis.com/css?family=Raleway:100,200,300,400');
+// Load core styles.
+require('_static/stylesheets/styles.scss');
 
-    // Background
-    background-color: #282C37;
-
-    // Font
-    font-family: 'Raleway', sans-serif;
-    font-size: 16px;
-    color: #FFFFFF;
-
-    // Remove default margin
-    margin: 0;
-
-    // Enable smooth scrolling.
-    scroll-behavior: smooth;
-  }
-`;
-
-const App = () => (
-  <React.Fragment>
-    <GlobalStyle />
-    <HomePage />
-  </React.Fragment>
-);
+const App = () => <HomePage />;
 
 ReactDOM.render(<App />, document.getElementById('app'));
